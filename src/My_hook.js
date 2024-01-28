471feba6b5e6c5333783290a67770bbdedf85006
valera2022 <113742376+valera2022@users.noreply.github.com>
2024-01-28 17:36:28 -0500

    Update My_hook.js

--- src/My_hook.js
+++ src/My_hook.js
@@ -2,32 +2,4 @@ import React from "react";
 import { useContext } from "react";
 import { AppContext } from "./context/context";
 
-export default function usePersonal(){
-    const {userInput,users} = useContext(AppContext)
-
-    // const rightUser = ()=>{
-        let user = users.find(user=>user.name === userInput)
-    //     return user
-         
-    // }
-
-    const isTeacher= ()=> {
-        // console.log(rightUser)
-        if(user.roll === "Teacher"){
-            return true
-        }
-        else{
-            return false
-        }
-    }
-    const isStudent= ()=>{
-        if(user.roll === "Student"){
-            return true
-        }
-        else{
-            return false
-        }
-    }
-    return {isTeacher,isStudent}
-}
 
